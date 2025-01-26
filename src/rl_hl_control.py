31ec88cc6a14e13476a4a61187e922f4b916e315
Sidharth Rajagopal <rsidharth53@gmail.com>
2025-01-26 14:19:13 -0800

    pass policy into rlhl controller

--- src/rl_hl_control.py
+++ src/rl_hl_control.py
@@ -218,10 +218,6 @@ class Hound_RLHL_Control:
 
 if __name__ == "__main__":
     rospy.init_node("hl_controller")
-    planner = Hound_RLHL_Control("circular_rift_model_4900", obs_type="blind")
-    # planner = Hound_RLHL_Control("1-24-25/radiant-yogurt-1347_model_8050.pt",
-    planner = Hound_RLHL_Control("1-25-25/comfy-night-1372_model_4950.pt",
-                                 obs_type="blind",
-                                 throttle_to_wheelspeed=3.0, rate=50,
-                                 hidden_shape=[64, 64])
+    policy = rospy.get_param("~policy")
+    planner = Hound_RLHL_Control(policy)
     rospy.spin()
