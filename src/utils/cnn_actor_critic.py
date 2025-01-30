e85c4fc275de148554efde2dfc6084c3a17fde8f
Yanda Bao <yandabaodav@gmail.com>
2025-01-29 19:50:22 -0800

    ported over Sanghun's arch changes

--- src/utils/cnn_actor_critic.py
+++ src/utils/cnn_actor_critic.py
@@ -14,8 +14,8 @@ class CNNActorCritic(nn.Module):
         actor_hidden_dims=[256, 256, 256],
         critic_hidden_dims=[256, 256, 256],
         image_shape=[40,80],
-        feature_dim=256,
-        activation="elu",
+        feature_dim=64,
+        activation="relu",
         init_noise_std=1.0,
         **kwargs,
     ):
@@ -27,17 +27,23 @@ class CNNActorCritic(nn.Module):
         super().__init__()
         activation = get_activation(activation)
 
+        self.feature_dim = feature_dim
         #feature extractor
         self.cnn = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=8, stride=4, padding=0),
+            nn.BathNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.BathNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(64, self.feature_dim, kernel_size=3, stride=1, padding=0),
+            nn.BathNorm2d(self.feature_dim),
             nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten(),
         )
 
+        """
         # Compute shape by doing one forward pass
         with torch.no_grad():
             n_flatten = self.cnn(torch.zeros(image_shape).unsqueeze(0).unsqueeze(0)).shape[1]
@@ -46,8 +52,7 @@ class CNNActorCritic(nn.Module):
             nn.Linear(n_flatten, feature_dim),
             nn.ReLU(),
         )
-
-        self.feature_dim = feature_dim
+        """
 
         self.num_additional_actor_obs = num_actor_obs - (image_shape[0] * image_shape[1])
         self.num_additional_critic_obs = num_critic_obs - (image_shape[0] * image_shape[1])
@@ -57,25 +62,29 @@ class CNNActorCritic(nn.Module):
 
         # Policy
         actor_layers = []
-        actor_layers.append(nn.Linear(mlp_input_dim_a, actor_hidden_dims[0]))
+        actor_layers.append(nn.Linear(mlp_input_dim_a, actor_hidden_dims[0]), bias=False)
+        actor_layers.append(nn.BatchNorm1d(actor_hidden_dims[0]))
         actor_layers.append(activation)
         for layer_index in range(len(actor_hidden_dims)):
             if layer_index == len(actor_hidden_dims) - 1:
-                actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], num_actions))
+                actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], num_actions, bias=True))
             else:
-                actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], actor_hidden_dims[layer_index + 1]))
+                actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], actor_hidden_dims[layer_index + 1], bias=False))
+                actor_layers.append(nn.BatchNorm1d(actor_hidden_dims[layer_index + 1]))
                 actor_layers.append(activation)
         self.actor = nn.Sequential(*actor_layers)
 
         # Value function
         critic_layers = []
-        critic_layers.append(nn.Linear(mlp_input_dim_c, critic_hidden_dims[0]))
+        critic_layers.append(nn.Linear(mlp_input_dim_c, critic_hidden_dims[0], bias=False))
+        critic_layers.append(nn.BatchNorm1d(critic_hidden_dims[0]))
         critic_layers.append(activation)
         for layer_index in range(len(critic_hidden_dims)):
             if layer_index == len(critic_hidden_dims) - 1:
-                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], 1))
+                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], 1, bias=True))
             else:
-                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], critic_hidden_dims[layer_index + 1]))
+                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], critic_hidden_dims[layer_index + 1], bias=False))
+                critic_layers.append(nn.BatchNorm1d(critic_hidden_dims[layer_index + 1]))
                 critic_layers.append(activation)
         self.critic = nn.Sequential(*critic_layers)
 
@@ -88,10 +97,26 @@ class CNNActorCritic(nn.Module):
         # disable args validation for speedup
         Normal.set_default_validate_args = False
 
+        self.initialize_weights()
+
         # seems that we get better performance without init
         # self.init_memory_weights(self.memory_a, 0.001, 0.)
         # self.init_memory_weights(self.memory_c, 0.001, 0.)
 
+    def initialize_weights(self):
+        """
+        Initialize Model Weights
+        """
+        for module in self.modules():
+            if isinstance(module, (nn.Conv2d, nn.Linear)):
+                nn.init.kaiming_normal_(module.weight, nonlinearity='relu')
+                if module.bias is not None:
+                    module.bias.data.zero_()
+            elif isinstance(module, (nn.BatchNorm2d, nn.BatchNorm1d)):
+                module.weight.data.fill_(1)
+                module.bias.data.zero_()
+
+    '''
     @staticmethod
     # not used at the moment
     def init_weights(sequential, scales):
@@ -99,6 +124,7 @@ class CNNActorCritic(nn.Module):
             torch.nn.init.orthogonal_(module.weight, gain=scales[idx])
             for idx, module in enumerate(mod for mod in sequential if isinstance(mod, nn.Linear))
         ]
+    '''
 
     def reset(self, dones=None):
         pass
@@ -122,8 +148,8 @@ class CNNActorCritic(nn.Module):
     def feature_extractor(self, observations):
         flat_images = observations[:, :-self.num_additional_actor_obs]
         images = flat_images.view(-1, 1, 40, 80)
-        out = self.cnn(images)
-        features = self.linear(out)
+        features = self.cnn(images)
+        ### features = self.linear(out)
         return features        
         
 
